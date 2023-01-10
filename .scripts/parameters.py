09730851587b2b8127896fae1d0adbf52aba29d0
Mal'dun Finsterschreck <Stefan Reiterer>
2023-01-10 03:17:24 +0100

    more galaxies added definied .gitignore

--- .scripts/parameters.py
+++ .scripts/parameters.py
@@ -129,6 +129,23 @@ shapes['dragon_tail'] = {SHAPE_NAME: 'Dragon Tail',
                          STARS_MIN_DIST:15.0
                          }
 
+# Fan (3)
+shapes['fan3'] = {SHAPE_NAME: 'Fan (3)',
+                         MIN_STARS: 1,
+                         COUNTRY: {MIN_SQ_DIST: 70**2,
+                                IDE_SQ_DIST: 120**2},
+                         FALLEN: {MIN_SQ_DIST:125**2,
+                                   IDE_SQ_DIST:175**2},
+                         ARMS: {TIGHT_WIND: 0.2,
+                                WIDTH: 30.0,
+                                FUZZ: 10.0,
+                                SEP: 120},
+                         NUM_ARMS: 3,
+                         CORE_RAD: 0.1,
+                         CORE_STARS: 0.1,
+                         STARS_MIN_DIST:15.0
+                         }
+
 # Fan (4)
 shapes['fan4'] = {SHAPE_NAME: 'Fan (4)',
                          MIN_STARS: 1,
@@ -136,8 +153,8 @@ shapes['fan4'] = {SHAPE_NAME: 'Fan (4)',
                                 IDE_SQ_DIST: 120**2},
                          FALLEN: {MIN_SQ_DIST:125**2,
                                    IDE_SQ_DIST:175**2},
-                         ARMS: {TIGHT_WIND: 0.1,
-                                WIDTH: 20.0,
+                         ARMS: {TIGHT_WIND: 0.2,
+                                WIDTH: 30.0,
                                 FUZZ: 10.0,
                                 SEP: 90},
                          NUM_ARMS: 4,
@@ -147,20 +164,55 @@ shapes['fan4'] = {SHAPE_NAME: 'Fan (4)',
                          }
                          
 
-# Star (4)
-shapes['star4'] = {SHAPE_NAME: 'Star (4)',
+# Snowflake (4)
+shapes['snowflake4'] = {SHAPE_NAME: 'Snowflake (4)',
                          MIN_STARS: 1,
                          COUNTRY: {MIN_SQ_DIST: 70**2,
                                 IDE_SQ_DIST: 120**2},
                          FALLEN: {MIN_SQ_DIST:125**2,
                                    IDE_SQ_DIST:175**2},
-                         ARMS: {TIGHT_WIND: 0.01,
-                                WIDTH: 10.0,
-                                FUZZ: 10.0,
+                         ARMS: {TIGHT_WIND: 0.0,
+                                WIDTH: 1.0,
+                                FUZZ: 1.0,
                                 SEP: 90},
                          NUM_ARMS: 4,
                          CORE_RAD: 0.3,
                          CORE_STARS: 0.3,
-                         STARS_MIN_DIST:15.0
+                         STARS_MIN_DIST:1.0
                          }
                          
+# Snowflake (6)
+shapes['snowflake6'] = {SHAPE_NAME: 'Snowflake (6)',
+                         MIN_STARS: 1,
+                         COUNTRY: {MIN_SQ_DIST: 70**2,
+                                IDE_SQ_DIST: 120**2},
+                         FALLEN: {MIN_SQ_DIST:125**2,
+                                   IDE_SQ_DIST:175**2},
+                         ARMS: {TIGHT_WIND: 0.0,
+                                WIDTH: 1.0,
+                                FUZZ: 1.0,
+                                SEP: 60},
+                         NUM_ARMS: 6,
+                         CORE_RAD: 0.3,
+                         CORE_STARS: 0.3,
+                         STARS_MIN_DIST:1.0
+                         }
+
+# Cross
+shapes['cross'] = {SHAPE_NAME: 'Cross',
+                         MIN_STARS: 1,
+                         COUNTRY: {MIN_SQ_DIST: 70**2,
+                                IDE_SQ_DIST: 120**2},
+                         FALLEN: {MIN_SQ_DIST:125**2,
+                                   IDE_SQ_DIST:175**2},
+                         ARMS: {TIGHT_WIND: 0.0,
+                                WIDTH: 20.0,
+                                FUZZ: 20.0,
+                                SEP: 90},
+                         NUM_ARMS: 4,
+                         CORE_RAD: 0.2,
+                         CORE_STARS: 0.2,
+                         STARS_MIN_DIST:1.0
+                         }
+
+
